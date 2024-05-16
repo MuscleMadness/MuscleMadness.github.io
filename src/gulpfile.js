abeeab5de61bc2df048141f649996b92bdce6e93
Ramaraj T <jsramraj@gmail.com>
2024-05-16 22:51:59 +0530

    use express to serve the page

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,16 +1,30 @@
- const { src, dest, watch, series } = require('gulp');
- const concat = require('gulp-concat')
- const sass = require('gulp-sass')(require('sass'));
+const { src, dest, watch, series } = require("gulp");
+const concat = require("gulp-concat");
+const sass = require("gulp-sass")(require("sass"));
 
- function buildStyles() {
-     return src('./styles/scss/**/*.scss')
-        .pipe(sass())
-        .pipe(concat('main.css'))
-        .pipe(dest('./styles/css'));
- }
+function buildStyles() {
+  return src("./styles/scss/**/*.scss")
+    .pipe(sass())
+    .pipe(concat("main.css"))
+    .pipe(dest("./styles/css"));
+}
 
- function watchTask() {
-     watch(['./styles/scss/**/*.scss'], buildStyles);
- }
+function watchTask() {
+  watch(["./styles/scss/**/*.scss"], buildStyles);
+}
 
- exports.default = series(buildStyles, watchTask);
+// Server task
+gulp.task("serve", function () {
+  connect.server({
+    root: ".", // Your project root
+    livereload: true,
+    port: 8080,
+  });
+
+  gulp.src("./index.html").pipe(open({ uri: "http://localhost:8080" }));
+});
+
+// Default task
+gulp.task("default", gulp.series("serve"));
+
+exports.default = series(buildStyles, watchTask);
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,21 @@
 import { mobileNavbar } from "./navbar.js";
-import { imageModal } from './modal.js';
+import { imageModal } from "./modal.js";
 
-document.addEventListener('DOMContentLoaded', () => {
-    mobileNavbar();
-    imageModal();
-})
+document.addEventListener("DOMContentLoaded", () => {
+  mobileNavbar();
+  imageModal();
+});
 
+var navItems = document.querySelectorAll(".nav__menu-item");
+
+navItems.forEach(function (item) {
+  item.addEventListener("click", function () {
+    // Remove 'active' class from all items
+    navItems.forEach(function (item) {
+      item.classList.remove("active");
+    });
+
+    // Add 'active' class to the clicked item
+    this.classList.add("active");
+  });
+});
