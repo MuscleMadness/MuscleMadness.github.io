a7015d22489ad53dd1ca1562acfa8f78ba520c9f
Ramaraj T <jsramraj@gmail.com>
2025-01-15 14:58:41 +0530

    add social media pages link workout planner

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,6 +6,29 @@ document.addEventListener("DOMContentLoaded", () => {
   imageModal();
 });
 
+const socialLinks = {
+  "fa-instagram": "https://www.instagram.com/muscle.madness.25/",
+  "fa-facebook-f":
+    "https://www.facebook.com/people/Muscle-Madness/61565273034156/",
+  "fa-youtube": "https://www.youtube.com/MuscleMadness25",
+  "fa-phone-alt": "tel:123456789",
+  "fa-envelope": "mailto:muscle.madness@gmail.com",
+};
+
+Object.keys(socialLinks).forEach((iconClass) => {
+  document.querySelectorAll(`.${iconClass}`).forEach((icon) => {
+    const url = socialLinks[iconClass];
+    const parent = icon.parentElement;
+    const link = document.createElement("a");
+    link.href = url;
+    link.target = "_blank";
+    link.rel = "noopener noreferrer";
+    link.classList.add("social-link");
+    parent.replaceChild(link, icon);
+    link.appendChild(icon);
+  });
+});
+
 var navItems = document.querySelectorAll(".nav__menu-item");
 
 navItems.forEach(function (item) {
